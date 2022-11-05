82b1eeec40ff2ec289bfe478e5e533ff033131e6
Vidhan Bhatt <me@vidhan.io>
2022-11-05 13:56:31 -0400

    chore: remove dead code and fix clippy warnings

--- src/stats/player/general/traditional.rs
+++ src/stats/player/general/traditional.rs
@@ -1,5 +1,3 @@
 pub mod fields {
-    use serde::{Deserialize, Serialize};
-
     pub use crate::stats::fields::{LeagueId, Season2022To1996 as Season, YesOrNo};
 }
--- src/stats/response/basic.rs
+++ src/stats/response/basic.rs
@@ -13,17 +13,3 @@ pub struct BasicResultSet {
     pub headers: Vec<String>,
     pub row_set: Vec<Vec<serde_json::Value>>,
 }
-
-impl BasicResultSet {
-    pub fn columns(
-        &self,
-    ) -> impl Iterator<Item = (&str, impl Iterator<Item = &serde_json::Value> + '_, usize)> {
-        self.headers.iter().enumerate().map(|(i, header)| {
-            (
-                &**header,
-                self.row_set.iter().map(move |row| &row[i]),
-                self.row_set.len(),
-            )
-        })
-    }
-}
